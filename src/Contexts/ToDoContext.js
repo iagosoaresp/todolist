1b327c5ffe37151edf52d43292eff83cda416c66
Francisco Iago <francisco.pereira@sequor.com.br>
2022-04-01 14:16:42 -0300

    first commit

---
+++ src/Contexts/ToDoContext.js
@@ -0,0 +1,52 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+
+export const ToDoContext = createContext()
+
+export const ToDoProvider = ({ children }) => {
+    const [listTasks, setListTasks] = useState([])
+    const [task, setTask] = useState("")
+
+    useEffect(() => { console.log(listTasks) }, [listTasks])
+    const addTask = async () => {
+        if (task !== "") {
+            const toDo = await {
+                task: task,
+                complete: false,
+                id: listTasks.length + 1
+            }
+            await setTask("")
+            await setListTasks([toDo, ...listTasks])
+        }
+    }
+
+    const changeTask = async (toDo) => {
+        await setTask(toDo)
+    }
+
+    const removeTask = async (key) => {
+        const remove = await listTasks.filter((task, index) => index === key ? null : task)
+        await setListTasks(remove)
+    }
+
+    const completeTask = async (id) => {
+        const complete = await listTasks.map((task) => task.id === id ? { ...task, complete: !task.complete } : { ...task })
+
+        complete.sort(function (a, b) {
+            return (a.complete > b.complete) ? 1 : ((b.complete > a.complete) ? -1 : 0);
+        })
+
+        await setListTasks(complete)
+    }
+
+    return (
+        <ToDoContext.Provider value={{ listTasks, setListTasks, addTask, task, changeTask, removeTask, completeTask }}>
+            {children}
+        </ToDoContext.Provider>
+    );
+}
+
+export function useToDo() {
+    const context = useContext(ToDoContext)
+
+    return context
+}
---
+++ src/Views/ToDo.js
@@ -0,0 +1,48 @@
+import '../styles/index.css'
+import React from 'react'
+import { FaPlusSquare, FaTrash, FaCheck, FaList } from "react-icons/fa";
+import { useToDo } from '../Contexts/ToDoContext';
+import logo from '../images/logo.png'
+
+export const ToDoList = () => {
+    const { listTasks, addTask, task, changeTask, removeTask, completeTask } = useToDo()
+    return (
+        <>
+            <div className='header'>
+                <img src={logo}/>
+            </div>
+            <div className='container'>
+                <div id="card">
+                    <div className='addTask'>
+                        <input className="form-control focus-input" placeholder='Digite a próxima tarefa' value={task} onChange={(e) => changeTask(e.target.value)} />
+                        <FaPlusSquare id="button-add" color='#420C59' fontSize={45} onClick={() => addTask()} />
+                    </div>
+                    <div className='container-tasks'>
+                        {
+                            listTasks.length <= 0 ?
+                                <div className='container-message'>
+                                    <FaList />
+                                    <span>Adicione uma tarefa</span>
+                                </div>
+                                :
+                                listTasks.map((task, index) => {
+                                    return (
+                                        <div className='list-tasks' style={{ backgroundColor: task.complete && "#00ff0047", transition: '1s' }} key={index}>
+                                            <button type='checkbox' style={{ backgroundColor: task.complete && "green" }} className='button-checkbox' value={task.complete} onClick={() => completeTask(task.id)}>
+                                                <FaCheck color='white' fontSize={10} display={!task.complete && 'none'} />
+                                            </button>
+                                            <span>{task.task}</span>
+                                            <FaTrash id='trash' onClick={() => removeTask(index)} />
+                                        </div>
+                                    )
+                                })
+                        }
+                    </div>
+                </div>
+            </div>
+        </>
+
+
+    )
+
+}
